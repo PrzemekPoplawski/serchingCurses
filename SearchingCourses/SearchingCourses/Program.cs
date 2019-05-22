91787144373b9ff54984c6aac9c40c44f536ade4
PrzemekPoplawski <przemyslaw.poplawski123@gmail.com>
2019-05-22 16:25:32 +0200

    json + classes

--- SearchingCourses/SearchingCourses/Program.cs
+++ SearchingCourses/SearchingCourses/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,20 +11,10 @@ namespace SearchingCourses
         static void Main(string[] args)
         {
             var songLyrics = new SongLyrics("Shakira", "Nada");
-
-            Console.WriteLine("Done");
+           
+            
+           Console.WriteLine("Done");
             Console.ReadLine();
-        }
-    }
-
-     class SongLyrics
-    {
-        public SongLyrics(string artist, string title)
-        {
-            var browser = new WebClient();
-            var url = "http://api.lyrics.ovh/v1/" + artist + "/" + title;
-            var json = browser.DownloadString(url);
-            Console.WriteLine(json);
         }
     }
 }
